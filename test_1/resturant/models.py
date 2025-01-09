17e08bd358af5b0f8943c235d467b73f934ffa7a
prawin <21ucs033@loyolacollege.edu>
2025-01-09 12:55:26 +0530

    created models

--- test_1/resturant/models.py
+++ test_1/resturant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Resturant(models.Model):
+    name = models.CharField(unique=True, max_length=100, primary_key=True)
+    address = models.CharField(max_length=500)
+    phone_num = models.CharField(max_length=10)
+
+class Reviews(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    resturant = models.ForeignKey(Resturant, on_delete= models.CASCADE)
+    
