33a8095843858479d1949b60e3f490689f3fd6b2
prawin <21ucs033@loyolacollege.edu>
2025-01-09 13:18:35 +0530

    views created in  views.py

--- test_1/resturant/views.py
+++ test_1/resturant/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json 
+from models import Resturant
 
 # Create your views here.
+@csrf_exempt
+def register(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        name = data['name']
+        address = data['address']
+        phone = data['phone']
+        new_resto =  Resturant(name=name, address=address, phone_num = phone)
+        new_resto.save()
+        return HttpResponse("Added sucessfully")
+    
