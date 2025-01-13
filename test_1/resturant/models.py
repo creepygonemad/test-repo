ef8615b0ec574e1dbae8877d112de27de66884be
prawin <21ucs033@loyolacollege.edu>
2025-01-13 13:14:45 +0530

    raitngs added in models

--- test_1/resturant/models.py
+++ test_1/resturant/models.py
@@ -5,8 +5,22 @@ class Resturant(models.Model):
     name = models.CharField(unique=True, max_length=100, primary_key=True)
     address = models.CharField(max_length=500)
     phone_num = models.CharField(max_length=10)
+    rating =models.SmallIntegerField(null=False, default=0)
+
+    def to_dict(self):
+        return{
+            "name":self.name,
+            "address":self.address,
+            "phone":self.phone_num,
+            "rating":self.rating
+        }
 
 class Reviews(models.Model):
     id = models.BigIntegerField(primary_key=True)
-    resturant = models.ForeignKey(Resturant, on_delete= models.CASCADE)
+    resturant = models.ForeignKey(Resturant, on_delete= models.CASCADE )
+
+    
+
+    
+       
     
